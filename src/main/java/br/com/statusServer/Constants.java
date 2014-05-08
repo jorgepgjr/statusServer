a1714d02f1a848a5a36fe37536397d1724c7b07e
Jorge Peres <jorgepgjr@hotmail.com>
2014-05-08 10:46:29 -0300

    mudando senha desenv

--- src/main/java/br/com/statusServer/Constants.java
+++ src/main/java/br/com/statusServer/Constants.java
@@ -2,6 +2,6 @@ package br.com.statusServer;
 
 public class Constants {
     public static final String userDesenv = "admwebabr";
-    public static final String passwordDesenv = "$abril789";
+    public static final String passwordDesenv = "dev123!";
     public static final String ipDesenv = "172.20.0.41";
 }
